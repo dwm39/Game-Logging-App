c0608d7df3f9c32a2904c16b93c57ff5f2436270
dwm39 <dwm39@pitt.edu>
2023-10-24 16:26:54 -0400

    what

---
+++ mvvm-flutter/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_flutter/views/movies_list_view.dart';
+import 'package:mvvm_flutter/viewmodels/movies_list_view_model.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.teal),
+        home: ChangeNotifierProvider(
+          create: (context) => MovieListViewModel(),
+          child: const BottomNavigationBarExample(),
+        ));
+  }
+}
+
+class BottomNavigationBarExampleApp extends StatelessWidget {
+  const BottomNavigationBarExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BottomNavigationBarExample(),
+    );
+  }
+}
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  final List<Widget> _widgetOptions = <Widget>[
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: ChangeNotifierProvider(
+          create: (context) => MovieListViewModel(),
+          child: const MovieListView(),
+        )),
+    const Scaffold(
+      body: Center(child: Text('This will be the add')),
+      // here will be two new view model and view
+    ),
+    const Scaffold(
+      body: Center(child: Text('This will be the Library')),
+      //same here
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('We Are Gaming'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Library',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
