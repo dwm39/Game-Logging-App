130029e4d2962bef85c7aec6838dbd80e19821cd
dwm39 <dwm39@pitt.edu>
2023-10-24 16:43:30 -0400

    please

--- mvvm-flutter/lib/web_service.dart
+++ mvvm-flutter/lib/web_service.dart
@@ -3,17 +3,14 @@ import 'package:mvvm_flutter/models/movie.dart';
 import 'package:http/http.dart' as http;
 
 class Webservice {
-
+  // why is the question
   Future<List<Movie>> fetchMovies(String keyword) async {
-
     String url = "http://www.omdbapi.com/?s=$keyword&apikey=eb0d5538";
     final response = await http.get(Uri.parse(url));
-    if(response.statusCode == 200) {
-
+    if (response.statusCode == 200) {
       final body = jsonDecode(response.body);
       final Iterable json = body["Search"];
       return json.map((movie) => Movie.fromJson(movie)).toList();
-
     } else {
       throw Exception("Unable to perform request!");
     }
