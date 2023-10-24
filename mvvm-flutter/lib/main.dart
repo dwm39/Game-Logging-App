1ec07c4e99b4c33dedbcfc2794cb93d9b83e28a7
dwm39 <dwm39@pitt.edu>
2023-10-24 16:51:24 -0400

    please be fine

--- mvvm-flutter/lib/main.dart
+++ mvvm-flutter/lib/main.dart
@@ -52,7 +52,7 @@ class _BottomNavigationBarExampleState
           child: const MovieListView(),
         )),
     const Scaffold(
-      body: Center(child: Text('This will be the add')),
+      body: Center(child: Text('This will be the add testing one more')),
       // here will be two new view model and view
     ),
     const Scaffold(
